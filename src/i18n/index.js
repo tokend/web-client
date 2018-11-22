7523eb98679c456f6550b2618e7401f6c8e1260d
Vladislav <logvinenko.js@gmail.com>
2018-11-22 13:06:13 +0200

    Move legacy translations to backwards

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,10 +1,12 @@
 import Polyglot from 'node-polyglot'
 import en from './en'
+import backwards from './backwards'
 
 const locale = 'en'
 const translations = { en }
 const polyglot = new Polyglot()
 
 polyglot.extend(translations[locale])
+polyglot.extend(backwards)
 
 export { polyglot }
