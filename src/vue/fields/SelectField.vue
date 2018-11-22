5bf2ecbdbadcb4ec36e453069c102bf84107582e
Vladislav <logvinenko.js@gmail.com>
2018-11-22 12:28:55 +0200

    Add custom select field and move script to mixin

--- src/vue/fields/SelectField.vue
+++ src/vue/fields/SelectField.vue
@@ -17,7 +17,7 @@
       @click.prevent="toggleListVisibility()"
     >
       <span class="select-field__selected-value">
-        {{ getValue(currentValue) || '&nbsp;' }}
+        {{ getLabel(currentValue) || '&nbsp;' }}
       </span>
       <div>
         <md-icon
@@ -33,160 +33,27 @@
       :class="{ 'select-field__list--active': isExpanded }"
     >
       <button
-        v-for="(val, i) in values"
+        v-for="(item, i) in values"
         :key="i"
         class="select-field__list-item"
-        :class="{ 'select-field__list-item--selected': selectedValue === val }"
-        @click.prevent="selectItem(val)"
+        :class="{
+          'select-field__list-item--selected':
+            getValue(selectedValue) === getValue(item)
+        }"
+        @click.prevent="selectItem(item)"
       >
-        {{ getValue(val) }}
+        {{ getLabel(item) }}
       </button>
     </div>
   </div>
 </template>
 
 <script>
-import { KEY_CODES } from '@/js/const/key-codes.const'
-import _isObject from 'lodash/isObject'
+import SelectFieldMixin from './js/select-field.mixin'
 
 export default {
   name: 'select-field',
-  props: {
-    value: {
-      type: [String, Number, Boolean, Object, Array, Date],
-      default: ''
-    },
-    values: {
-      type: Array,
-      default: _ => []
-    },
-    label: {
-      type: String,
-      default: ''
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    }
-  },
-
-  data: _ => ({
-    currentValue: null, // selected item in the list
-    selectedValue: null, // active list element (for arrow navigation)
-    isExpanded: false,
-    KEY_CODES
-  }),
-
-  watch: {
-    isExpanded (value) {
-      if (value) {
-        document.addEventListener('click', this.onDocumentClick)
-      }
-      // closeElement('select__list', value, this.closeList)
-    }
-  },
-
-  created () {
-    this.selectedValue = this.value
-    this.currentValue = this.value
-
-    document.addEventListener('keydown', this.onDocumentKeyDown)
-  },
-
-  destroyed () {
-    document.removeEventListener('keydown', this.onDocumentKeyDown)
-  },
-
-  methods: {
-    getValue (item) {
-      return _isObject(item) ? item.label : item
-    },
-    selectItem (item) {
-      this.selectedValue = item
-      this.currentValue = item
-      this.$emit('input', this.getValue(item))
-      this.toggleListVisibility()
-    },
-    toggleListVisibility () {
-      this.isExpanded ? this.closeList() : this.openList()
-    },
-    openList () {
-      const index = this.getIndex(this.currentValue)
-
-      this.scrollList(index)
-      this.isExpanded = true
-    },
-    closeList () {
-      this.selectedValue = this.currentValue // set active element as selected
-      this.isExpanded = false
-    },
-    onDocumentClick (event) {
-      if (!event.target.closest('.select__list')) {
-        this.closeList()
-        document.removeEventListener('click', this.onDocumentClick)
-      }
-    },
-    onDocumentKeyDown (event) {
-      if (!this.isExpanded) {
-        return
-      }
-
-      let index = this.getIndex(this.selectedValue)
-
-      switch (event.which) {
-        case KEY_CODES.enter:
-          this.selectItem(this.values[index])
-          break
-        case KEY_CODES.up:
-          index = this.selectPrevItem(index, this.values)
-          break
-        case KEY_CODES.right:
-          this.selectItem(this.values[index])
-          break
-        case KEY_CODES.down:
-          index = this.selectNextItem(index, this.values)
-          break
-        case KEY_CODES.escape:
-          this.toggleListVisibility()
-          break
-        case KEY_CODES.tab:
-          if (event.shiftKey) {
-            index = this.selectPrevItem(index, this.values)
-          } else {
-            index = this.selectNextItem(index, this.values)
-          }
-          break
-        default:
-          return
-      }
-
-      this.scrollList(index)
-    },
-    getIndex (item) {
-      if (!_isObject(item)) {
-        return this.values.findIndex(entry => entry.value === item.value)
-      }
-      return this.values.indexOf(item)
-    },
-    scrollList (index) {
-      const list = this.$refs.list
-
-      if (index !== -1) {
-        list.scrollTop =
-          list.childNodes[index].offsetTop - (list.offsetHeight / 2) + 18
-      }
-    },
-    selectNextItem (index, valuesList) {
-      index === valuesList.length - 1 ? index = 0 : index += 1
-      this.selectedValue = valuesList[index]
-      return index
-    },
-    selectPrevItem (index, valuesList) {
-      index === 0 ? index += valuesList.length - 1 : index -= 1
-      this.selectedValue = valuesList[index]
-      return index
-    }
-  }
+  mixins: [SelectFieldMixin]
 }
 </script>
 
