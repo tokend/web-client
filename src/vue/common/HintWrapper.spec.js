7623aafad2e42e27f9d9a6c4895a07ef05961143
Vladislav <logvinenko.js@gmail.com>
2018-11-26 16:25:00 +0200

    Add test for --decorated HintWrapper modifier

--- src/vue/common/HintWrapper.spec.js
+++ src/vue/common/HintWrapper.spec.js
@@ -12,4 +12,23 @@ describe('HintWrapper component test', () => {
 
     expect(renderedHint).to.equal(hint)
   })
+
+  it('template has the --decorated modifier by default', () => {
+    const hint = 'My awesome hint'
+    const wrapper = mount(HintWrapper, { propsData: { hint } })
+
+    expect(wrapper.classes()).to.include('hint-wrapper--decorated')
+  })
+
+  it('template doesn\'t have the --decorated modifier if the we said so', () => {
+    const hint = 'My awesome hint'
+    const wrapper = mount(HintWrapper, {
+      propsData: {
+        hint,
+        decorated: false
+      }
+    })
+
+    expect(wrapper.classes()).to.not.include('hint-wrapper--decorated')
+  })
 })
