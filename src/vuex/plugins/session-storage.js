791533393b5e4488ddd741da9fc43e900ff5c873
Vladislav <logvinenko.js@gmail.com>
2018-11-28 16:52:16 +0200

    Clean-up session-storage plugin

--- src/vuex/plugins/session-storage.js
+++ src/vuex/plugins/session-storage.js
@@ -1,12 +1,8 @@
-// import { vuexTypes } from '@/vuex/types'
 // TODO: tmp using old types until the migration to new modules complete
 import { vuexTypes as LVuexTypes } from 'L@/vuex/types'
 
 const config = Object.freeze({
-  STORAGE_KEY: 'TokenDStore-v2',
-  CLEAR_ON_MUTATIONS: [LVuexTypes.SET_LOGGED_OUT_STATE],
-  RESTORE_ON_MUTATIONS: [LVuexTypes.POP_STATE],
-  DONT_SAVE_ON_MUTATIONS: [LVuexTypes.KEEP_SESSION]
+  STORAGE_KEY: 'TokenDStore-v2'
 })
 
 export const sessionStoragePlugin = store => {
