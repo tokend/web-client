c6f6f0d03e756cb357d915d05356f6c9e896a419
Vladislav <logvinenko.js@gmail.com>
2018-11-29 17:10:36 +0200

    Add logo component

---
+++ src/vue/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-link class="logotype" to="/dashboard">
+    <img src="/static/logo.svg" alt="TokenD">
+  </router-link>
+</template>
+
+<script>
+export default { name: 'logo' }
+</script>
