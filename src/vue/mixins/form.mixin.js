a392f2f75df57d5870b7f59ba69f0b1548b12a7e
Vladislav <logvinenko.js@gmail.com>
2018-12-06 15:44:49 +0200

    Maintain validation errors flow

--- src/vue/mixins/form.mixin.js
+++ src/vue/mixins/form.mixin.js
@@ -1,6 +1,10 @@
 import { validationMixin } from 'vuelidate'
 
 import InputField from '../fields/InputField'
+import { Bus } from '@/js/helpers/event-bus'
+import { globalize } from '@/vue/filters/globalize'
+
+import safeGet from 'lodash/get'
 
 export default {
   components: {
@@ -15,7 +19,30 @@ export default {
   methods: {
     isFormValid () {
       this.$v.$touch()
-      return !this.$v.$invalid
+      const isValid = !this.$v.$invalid
+      if (!isValid) {
+        Bus.error('validation.failed')
+      }
+      return isValid
+    },
+    errorMessage (field) {
+      if (!this.$v.$invalid) {
+        return ''
+      }
+
+      const fieldDetails = safeGet(this.$v, field)
+
+      if (!fieldDetails.$dirty) {
+        return ''
+      }
+
+      for (const rule of Object.keys(fieldDetails.$params)) {
+        if (!fieldDetails[rule]) {
+          return globalize(`validation.field-error`, {
+            context: rule
+          })
+        }
+      }
     },
     disableForm () {
       this.formMixin.isDisabled = true
