7c27476824d4e51657cad39b8518921f3123a641
Vladislav <logvinenko.js@gmail.com>
2018-12-11 14:14:41 +0200

    Do not import * validators when we need only one

--- src/validators.js
+++ src/validators.js
@@ -1,6 +1,6 @@
 import { base } from '@tokend/js-sdk'
 import * as validators from 'vuelidate/lib/validators'
-export * from 'vuelidate/lib/validators'
+export { minLength } from 'vuelidate/lib/validators'
 
 export const password = value => validators.minLength(6)(value)
 export const seed = value => base.Keypair.isValidSecretKey(value)
