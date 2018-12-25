ac4fa845615bed08cf2d38e104373ff5ca8a7596
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-12-25 18:52:05 +0200

    Fix script

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+set -ex
+
 docker build --pull -t $CI_REGISTRY_IMAGE:latest .
 docker push $CI_REGISTRY_IMAGE:latest
