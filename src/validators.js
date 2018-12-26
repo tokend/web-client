18065a464a7b51d8a54221dcfd22a2ba5e8701da
Dmitry Sviridenko <letehaha@gmail.com>
2018-12-26 21:01:14 +0200

    Add custom validation rules

--- src/validators.js
+++ src/validators.js
@@ -1,8 +1,11 @@
 import { base } from '@tokend/js-sdk'
 import * as validators from 'vuelidate/lib/validators'
-export { minLength } from 'vuelidate/lib/validators'
 
 export const password = value => validators.minLength(6)(value)
 export const seed = value => base.Keypair.isValidSecretKey(value)
+export const amount = value => Number(value) && Number(value) > 0
+export const maxValueWrapper = value => {
+  return !validators.helpers.req(value()) || validators.maxValue(value())
+}
 
 export * from 'vuelidate/lib/validators'
