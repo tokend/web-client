865a5eba91ee2669304769185042745a85538892
Dmitry Sviridenko <letehaha@gmail.com>
2018-12-26 21:03:08 +0200

    Add issuance creation form

--- src/vue/forms/CreateIssuanceForm.vue
+++ src/vue/forms/CreateIssuanceForm.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="app__page-content-wrp">
     <template v-if="accountTypeI !== ACCOUNT_TYPES.syndicate">
-      <h2 class="app__page-heading">{{ i18n.iss_not_available_heading() }}</h2>
+      <h2 class="app__page-heading">
+        {{ 'create-issuance-form.not-available-heading' | globalize }}
+      </h2>
       <p class="app__page-explanations app__page-explanations--secondary">
-        {{ i18n.iss_not_available() }}
+        {{ 'create-issuance-form.not-available' | globalize }}
       </p>
       <router-link
         to="/verification"
         tag="button"
         class="app__button-raised">
-        {{ i18n.iss_kyc_btn() }}
+        {{ 'create-issuance-form.to-verification-btn' | globalize }}
       </router-link>
     </template>
 
     <template v-else-if="!accountOwnedTokenCodes.length">
-      <h2 class="app__page-heading">{{ i18n.iss_no_assets_heading() }}</h2>
+      <h2 class="app__page-heading">
+        {{ 'create-issuance-form.no-assets-heading' | globalize }}
+      </h2>
       <p class="app__page-explanations app__page-explanations--secondary">
-        {{ i18n.iss_no_assets() }}
+        {{ 'create-issuance-form.no-assets' | globalize }}
       </p>
       <router-link
         to="/token-creation"
         tag="button"
         class="app__button-raised">
-        {{ i18n.iss_create_assets_btn() }}
+        {{ 'create-issuance-form.create-token-btn' | globalize }}
       </router-link>
     </template>
 
@@ -32,35 +36,32 @@
         @submit.prevent="submit"
         v-if="formShown">
         <h2 class="app__page-heading">
-          Issuance form
+          {{ 'create-issuance-form.issuance-form' | globalize }}
         </h2>
 
         <div class="app__form-row">
-          <select-field-unchained
+          <select-field
             :values="accountOwnedTokenCodes"
             v-model="request.code"
-            :label="i18n.lbl_asset()" />
+            :label="'create-issuance-form.asset-lbl' | globalize"
+          />
         </div>
 
         <div class="app__form-row">
           <div class="app__form-field">
-            <input-field-unchained
+            <input-field
               name="amount"
               v-model="request.amount"
               type="number"
               :max="selectedTokenAvailableToIssuance"
-              v-validate="{
-                required: true,
-                amount: true,
-                max_value: selectedTokenAvailableToIssuance
-              }"
-              :label="i18n.lbl_amount()"
-              :error-message="errorMessage('amount')"
+              :label="'create-issuance-form.amount-lbl' | globalize"
+              @blur="_touchField('request.amount')"
+              :error-message="_getErrorMessage('request.amount')"
             />
             <div class="app__form-field-description">
               <p>
                 {{
-                  i18n.iss_available_to_issuance({
+                  'create-issuance-form.available-to-issuance' | globalize({
                     value: selectedTokenAvailableToIssuance,
                     tokenCode: request.code
                   })
@@ -71,23 +72,23 @@
         </div>
 
         <div class="app__form-row">
-          <input-field-unchained
+          <input-field
             id="token-name"
             name="issuance email"
             v-model="request.receiver"
-            v-validate="'required|email'"
-            :label="i18n.lbl_email()"
-            :error-message="errorMessage('issuance email')"
+            :label="'create-issuance-form.email-lbl' | globalize"
+            @blur="_touchField('request.receiver')"
+            :error-message="_getErrorMessage('request.receiver')"
           />
         </div>
 
         <div class="app__form-row">
-          <input-field-unchained
+          <input-field
             name="reference"
             v-model="request.reference"
-            v-validate="'required'"
-            :label="i18n.lbl_reference()"
-            :error-message="errorMessage('reference')"
+            :label="'create-issuance-form.reference-lbl' | globalize"
+            @blur="_touchField('request.reference')"
+            :error-message="_getErrorMessage('request.reference')"
           />
         </div>
 
@@ -95,9 +96,8 @@
           <button
             v-ripple
             type="submit"
-            class="app__form-submit-btn"
-            :disabled="isPending">
-            {{ i18n.iss_issue_btn() }}
+            class="app__form-submit-btn">
+            {{ 'create-issuance-form.issue-btn' | globalize }}
           </button>
         </div>
       </form>
@@ -106,33 +106,47 @@
 </template>
 
 <script>
-import FormMixin from 'L@/vue/common/mixins/form.mixin'
-import SelectFieldUnchained from 'L@/vue/common/fields/SelectFieldUnchained'
-import InputFieldUnchained from 'L@/vue/common/fields/InputFieldUnchained'
-import { i18n } from 'L@/js/i18n'
+import FormMixin from '@/vue/mixins/form.mixin'
 import { mapGetters } from 'vuex'
+// FIXME: change it to actual
 import { vuexTypes } from 'L@/vuex/types'
 import { EventDispatcher } from 'L@/js/events/event_dispatcher'
 import { ErrorHandler } from 'L@/js/errors/error_handler'
 import { issuanceService } from 'L@/js/services/issuances.service'
 import { accountsService } from 'L@/js/services/accounts.service'
-import { ACCOUNT_TYPES } from 'L@/js/const/const'
+// FIXME: move XDR-dependent object imports to sdk
+import { ACCOUNT_TYPES } from '@/js/const/xdr.const'
 import { errors } from 'L@/js/errors/factory'
+import { required, email, amount } from '@validators'
+import { globalize } from '@/vue/filters/globalize'
 
 export default {
-  name: 'create-issuance',
-  components: { SelectFieldUnchained, InputFieldUnchained },
+  name: 'create-issuance-form',
   mixins: [FormMixin],
-  data: _ => ({
+  data: () => ({
     request: {
       code: ''
     },
     formShown: true,
     unissued: '', // TODO: unissued amount label, exceeding check
     selectedTokenAvailableToIssuance: null,
-    i18n,
     ACCOUNT_TYPES
   }),
+  validations: {
+    request: {
+      amount: {
+        required,
+        amount
+        // TODO: fix it
+        // maxValueWrapper: maxValueWrapper(() => {
+        //   console.log('this', this)
+        //   return +this.selectedTokenAvailableToIssuance
+        // })
+      },
+      receiver: { required, email },
+      reference: { required }
+    }
+  },
   computed: {
     ...mapGetters([
       vuexTypes.accountOwnedTokens,
@@ -144,18 +158,18 @@ export default {
   },
   watch: {
     'request.code' (value) {
-      this.getAvailableToIssuance(value)
+      if (this.request.code) this.getAvailableToIssuance(value)
     }
   },
   created () {
     this.setTokenCode()
-    if (this.request) {
+    if (this.request.code) {
       this.getAvailableToIssuance(this.request.code)
     }
   },
   methods: {
     async submit () {
-      if (!await this.isValid()) return
+      if (!await this._isFormValid()) return
       this.disable()
       try {
         const receiver = await accountsService.loadBalanceIdByEmail(
@@ -169,12 +183,16 @@ export default {
           reference: this.request.reference,
           externalDetails: {}
         })
-        EventDispatcher.dispatchShowSuccessEvent(i18n.iss_submit_success())
+        EventDispatcher.dispatchShowSuccessEvent(
+          globalize('create-issuance-form.submit-success')
+        )
         this.rerenderForm()
       } catch (error) {
         if (error.constructor === errors.NotFoundError) {
           EventDispatcher.dispatchShowErrorEvent(
-            i18n.iss_no_balance({ asset: this.request.code })
+            globalize('create-issuance-form.no-balance', {
+              asset: this.request.code
+            })
           )
           this.enable()
           return
@@ -202,3 +220,7 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  @import './app-form';
+</style>
