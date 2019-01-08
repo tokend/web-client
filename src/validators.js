a0133fae1b5626be93609d6da86322cf53ea8554
Dmitry Sviridenko <letehaha@gmail.com>
2019-01-08 17:26:45 +0200

    Add emailOrAccountID field validation

--- src/validators.js
+++ src/validators.js
@@ -7,5 +7,13 @@ export const amount = value => Number(value) && Number(value) > 0
 export const maxValueWrapper = value => {
   return !validators.helpers.req(value()) || validators.maxValue(value())
 }
+export const emailOrAccountId = value => {
+  return validateEmail(value) || base.Keypair.isValidPublicKey(value)
+}
 
 export * from 'vuelidate/lib/validators'
+
+export function validateEmail (email) {
+  const reg = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?')
+  return reg.test(email)
+}
