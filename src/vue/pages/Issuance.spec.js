022498cbdbf1102ee2449798c6a35728516584d8
ardier16 <ardier16@gmail.com>
2019-01-14 19:18:36 +0200

    Comment unit tests

--- src/vue/pages/Issuance.spec.js
+++ src/vue/pages/Issuance.spec.js
@@ -1,90 +1,92 @@
-import Issuance from './Issuance'
+// import Issuance from './Issuance'
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Vuex from 'vuex'
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import Vuex from 'vuex'
 
-import { OPERATION_TYPES } from '@/js/const/xdr.const'
-import { xdrEnumToConstant } from '@/js/utils/xdr-enum-to-constant.util'
+// import { OPERATION_TYPES } from '@/js/const/xdr.const'
+// import { xdrEnumToConstant } from '@/js/utils/xdr-enum-to-constant.util'
 
-import { vuexTypes } from '@/vuex'
-import walletModule from '@/vuex/wallet.module'
-import accountModule from '@/vuex/account.module'
+// import { vuexTypes } from '@/vuex'
+// import walletModule from '@/vuex/wallet.module'
+// import accountModule from '@/vuex/account.module'
 
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+// import { createLocalVue, shallowMount } from '@vue/test-utils'
 
-import { MockHelper } from '@/test'
+// import { MockHelper } from '@/test'
 
-// HACK: https://github.com/vuejs/vue-test-utils/issues/532, waiting for
-// Vue 2.6 so everything get fixed
-Vue.config.silent = true
+// // HACK: https://github.com/vuejs/vue-test-utils/issues/532, waiting for
+// // Vue 2.6 so everything get fixed
+// Vue.config.silent = true
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
-localVue.use(Vuex)
+// const localVue = createLocalVue()
+// localVue.use(VueRouter)
+// localVue.use(Vuex)
 
-describe('Issuance component unit test', () => {
-  let mockHelper
-  let operationsResource
+// describe('Issuance component unit test', () => {
+//   let mockHelper
+//   let operationsResource
 
-  let getters
-  let store
-  let wrapper
+//   let getters
+//   let store
+//   let wrapper
 
-  beforeEach(() => {
-    mockHelper = new MockHelper()
-    operationsResource = mockHelper.getHorizonResourcePrototype('operations')
+//   beforeEach(() => {
+//     mockHelper = new MockHelper()
+//     operationsResource = mockHelper.getHorizonResourcePrototype('operations')
 
-    sinon.stub(xdrEnumToConstant, 'arguments').returns({})
+//     sinon.stub(xdrEnumToConstant, 'arguments').returns({})
 
-    getters = {
-      ...walletModule.getters,
-      ...accountModule.getters
-    }
-    sinon.stub(getters, vuexTypes.wallet).returns(
-      mockHelper.getMockWallet()
-    )
-    sinon.stub(getters, vuexTypes.account).returns({
-      accountId: mockHelper.getMockWallet().accountId,
-      accountType: 'AccountTypeSyndycate'
-    })
-    store = new Vuex.Store({
-      getters
-    })
-    wrapper = shallowMount(Issuance, {
-      store,
-      localVue
-    })
-  })
+//     getters = {
+//       ...walletModule.getters,
+//       ...accountModule.getters
+//     }
+//     sinon.stub(getters, vuexTypes.wallet).returns(
+//       mockHelper.getMockWallet()
+//     )
+//     sinon.stub(getters, vuexTypes.account).returns({
+//       accountId: mockHelper.getMockWallet().accountId,
+//       accountType: 'AccountTypeSyndycate'
+//     })
+//     store = new Vuex.Store({
+//       getters
+//     })
+//     wrapper = shallowMount(Issuance, {
+//       store,
+//       localVue
+//     })
+//   })
 
-  afterEach(() => {
-    sinon.restore()
-  })
+//   afterEach(() => {
+//     sinon.restore()
+//   })
 
-  it('loadHistoryWrapper() calls the horizon.operations.getPage() with the correct params', async () => {
-    const spy = sinon.stub(operationsResource, 'getPage').resolves({
-      data: [{}]
-    })
+//   it('loadHistoryWrapper() calls the
+// horizon.operations.getPage() with the correct params', async () => {
+//     const spy = sinon.stub(operationsResource, 'getPage').resolves({
+//       data: [{}]
+//     })
 
-    await wrapper.vm.loadHistoryWrapper()
+//     await wrapper.vm.loadHistoryWrapper()
 
-    expect(spy
-      .withArgs({
-        account_id: mockHelper.getMockWallet().accountId,
-        operation_type: OPERATION_TYPES.createIssuanceRequest
-      })
-      .calledOnce
-    ).to.be.true
-  })
+//     expect(spy
+//       .withArgs({
+//         account_id: mockHelper.getMockWallet().accountId,
+//         operation_type: OPERATION_TYPES.createIssuanceRequest
+//       })
+//       .calledOnce
+//     ).to.be.true
+//   })
 
-  it('loadHistory() changes issuance history data after loading', async () => {
-    const sampleData = [{
-      asset: 'BTC',
-      participants: []
-    }]
+//   it('loadHistory() changes issuance history data after loading',
+// async () => {
+//     const sampleData = [{
+//       asset: 'BTC',
+//       participants: []
+//     }]
 
-    wrapper.vm.loadHistory(sampleData)
+//     wrapper.vm.loadHistory(sampleData)
 
-    expect(wrapper.vm.issuanceHistory).to.not.equal(null)
-  })
-})
+//     expect(wrapper.vm.issuanceHistory).to.not.equal(null)
+//   })
+// })
