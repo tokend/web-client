4db9b7c114d6f2e689fdde866fb54003138bac91
Dmitry Sviridenko <letehaha@gmail.com>
2019-01-21 15:17:25 +0200

    Fix eslint issue

--- envs/local.js
+++ envs/local.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"default"',
   HORIZON_SERVER: '"http://localhost:8000"',
   FILE_STORAGE: '"http://localhost:9000"',
-  NETWORK_PASSPHRASE: '"TokenD Developer Network"'
+  NETWORK_PASSPHRASE: '"TokenD Developer Network"',
 }
