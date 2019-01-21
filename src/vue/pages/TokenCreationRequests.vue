b6e733d09e31752496cc1ef299ea225af1368307
ardier16 <ardier16@gmail.com>
2019-01-21 20:03:39 +0200

    Fix request state displaying
    + show request state from the translations
    + colored dots for request states

--- src/vue/pages/TokenCreationRequests.vue
+++ src/vue/pages/TokenCreationRequests.vue
@@ -30,7 +30,12 @@
             :key="i"
           >
             <td>{{ request.assetDetails.code }}</td>
-            <td>{{ request.requestState }}</td>
+            <td
+              class="request-state"
+              :class="`request-state--${request.requestState}`"
+            >
+              {{ getRequestStateMessage(request.requestState) }}
+            </td>
             <td>{{ request.createdAt | formatCalendar }}</td>
             <td>{{ request.updatedAt | formatCalendar }}</td>
             <td>
@@ -58,10 +63,14 @@
 
 <script>
 import Loader from '@/vue/common/Loader'
-import Drawer from '@/vue//common/Drawer'
+import Drawer from '@/vue/common/Drawer'
 import TokenRequestDetails from '@/vue/common/TokenRequestDetails'
 
 import { Sdk } from '@/sdk'
+
+import { REQUEST_STATES_STR } from '@/js/const/request-states.const'
+
+import { globalize } from '@/vue/filters/globalize'
 
 import { mapGetters } from 'vuex'
 import { vuexTypes } from '@/vuex'
@@ -110,6 +119,22 @@ export default {
       this.selectedRequest = request
       this.isDetailsDrawerShown = true
     },
+    getRequestStateMessage (requestState) {
+      switch (requestState) {
+        case REQUEST_STATES_STR.pending:
+          return globalize('requests-page.request-pending-msg')
+        case REQUEST_STATES_STR.approved:
+          return globalize('requests-page.request-approved-msg')
+        case REQUEST_STATES_STR.rejected:
+          return globalize('requests-page.request-rejected-msg')
+        case REQUEST_STATES_STR.cancelled:
+          return globalize('requests-page.request-cancelled-msg')
+        case REQUEST_STATES_STR.permanentlyRejected:
+          return globalize('requests-page.request-permanently-rejected-msg')
+        default:
+          return ''
+      }
+    },
   },
 }
 </script>
@@ -120,11 +145,51 @@ export default {
 .token-creation-requests__table {
   overflow-x: auto;
   box-shadow: 0 0.6rem 1rem 0 $col-table-shadow;
+
+  tr td:last-child {
+    text-align: right;
+  }
 }
 
 .request-details-btn {
   font-size: 1.2rem;
   color: $col-primary-lighten;
   cursor: pointer;
+}
+
+.request-state {
+  padding-left: 3rem;
+  text-transform: capitalize;
+
+  &--approved,
+  &--pending,
+  &--rejected {
+    position: relative;
+
+    &:before {
+      content: "";
+      position: absolute;
+      width: 0.6rem;
+      height: 0.6rem;
+      top: 1.7rem;
+      transform: translateY(-50%);
+      left: 1.6rem;
+      border-radius: 100%;
+    }
+  }
+
+  &--approved:before {
+    background-color: $col-success;
+  }
+
+  &--pending:before {
+    background-color: $col-warning;
+  }
+
+  &--rejected:before,
+  &--cancelled:before,
+  &--permanentlyRejected:before {
+    background-color: $col-error;
+  }
 }
 </style>
