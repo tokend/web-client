b21b5c9f6b37e6a40167d46c1c8cf24f8351cb89
Dmitry Sviridenko <letehaha@gmail.com>
2019-01-22 20:05:21 +0200

    Replace inlined colors to variables in Sidebar

--- src/vue/navigation/Sidebar.vue
+++ src/vue/navigation/Sidebar.vue
@@ -137,7 +137,7 @@ $content-item-right-padding: 2.4rem;
   transform: translateX($sidebar-width);
   opacity: 0;
   cursor: pointer;
-  color: #fff;
+  color: $col-sidebar-burger-icon-color;
   transition: opacity 0.15s cubic-bezier(0.4, 0, 0.2, 1),
     transform 0.25s cubic-bezier(0.4, 0, 0.2, 1);
 
