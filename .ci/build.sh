bfbd5ab5811f32be325899c4bf47c197bb96ee2c
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2019-01-28 16:33:33 +0200

    Fix build

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-docker build --pull -t $CI_REGISTRY_IMAGE:latest .
-docker push $CI_REGISTRY_IMAGE:latest
+docker build --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
+docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA
