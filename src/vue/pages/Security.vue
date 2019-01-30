a0382d130917517f05c93fbcbeae3c09ed83cb2d
ardier16 <ardier16@gmail.com>
2019-01-30 12:11:33 +0200

    Security page design

--- src/vue/pages/Security.vue
+++ src/vue/pages/Security.vue
@@ -5,9 +5,35 @@
         Enable 2FA
       </p>
       <a class="security__row-action">
+        Switcher
+      </a>
+    </div>
+    <hr class="security__line">
+    <div class="security__row">
+      <p class="security__row-title">
+        Password
+      </p>
+      <a class="security__row-action">
         Change password
       </a>
-      <hr>
+    </div>
+    <hr class="security__line">
+    <div class="security__row">
+      <p class="security__row-title">
+        Account ID
+      </p>
+      <a class="security__row-action">
+        View Account ID
+      </a>
+    </div>
+    <hr class="security__line">
+    <div class="security__row">
+      <p class="security__row-title">
+        Secret seed
+      </p>
+      <a class="security__row-action">
+        View secret seed
+      </a>
     </div>
   </div>
 </template>
@@ -31,8 +57,29 @@ export default {
 @import "~@scss/mixins";
 
 .security {
-  background: #fff;
-  padding: 2.4rem;
+  background: $col-block-bg;
   @include box-shadow();
+
+  .security__row {
+    padding: 2.4rem;
+    display: flex;
+    justify-content: space-between;
+
+    .security__row-title {
+      font-size: 1.8rem;
+      color: $col-primary;
+    }
+
+    .security__row-action {
+      font-size: 1.3rem;
+      cursor: pointer;
+      color: $col-primary-lighten;
+    }
+  }
+
+  .security__line {
+    margin: 0 2.4rem;
+    border: $col-block-line solid .05rem;
+  }
 }
 </style>
