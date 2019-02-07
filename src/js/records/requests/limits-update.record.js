7dd951a911b8f42b2a09aa50d9bac43a92e50ec7
Dmitry Sviridenko <letehaha@gmail.com>
2019-02-07 13:34:26 +0200

    Add requestedDocs getter to limits-update.record

--- src/js/records/requests/limits-update.record.js
+++ src/js/records/requests/limits-update.record.js
@@ -8,4 +8,33 @@ export class LimitsUpdateRequestRecord extends RequestRecord {
     this.requestType = _get(record, 'details.limitsUpdate.details.requestType')
     this.asset = _get(record, 'details.limitsUpdate.details.asset')
   }
+
+  isValidJSONString (str) {
+    try {
+      JSON.parse(str)
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+
+  get requestedDocs () {
+    let requestedDocs = []
+
+    // user may have requested docs only when request state is 'rejected'
+    // eslint-disable-next-line
+    if (this.state === 'rejected' && this.isValidJSONString(this._record.rejectReason)) {
+      const rejectReason = JSON.parse(this._record.rejectReason)
+
+      if (Object.keys(rejectReason).includes('docsToUpload')) {
+        requestedDocs = rejectReason.docsToUpload
+        // replace document-types from 'some type' to 'some_type' to allow
+        // translations using
+        requestedDocs.forEach(item => {
+          item.label = item.label.replace(/ /g, '_')
+        })
+      }
+    }
+    return requestedDocs
+  }
 }
