293bae396ca21f7cba87f83acc9820fae7c046ed
Dmitry Sviridenko <letehaha@gmail.com>
2019-02-08 20:18:03 +0200

    Add maxValueWrapper and maxDecimalPoints validator rules

--- src/validators.js
+++ src/validators.js
@@ -7,15 +7,24 @@ import * as validators from 'vuelidate/lib/validators'
 export const password = value => validators.minLength(6)(value)
 export const seed = value => base.Keypair.isValidSecretKey(value)
 export const amount = value => Number(value) && Number(value) > 0
-export const maxValueWrapper = value => {
-  return !validators.helpers.req(value()) || validators.maxValue(value())
+export const maxValueWrapper = maxValue => value => {
+  return Number(value) < Number(maxValue)
+}
+export const maxDecimalPoints = points => value => {
+  const splittedValue = value.split('.')
+  if (splittedValue.length < 2) {
+    return true
+  } else {
+    return splittedValue[splittedValue.length - 1].length <= Number(points)
+  }
 }
 export const emailOrAccountId = value => {
   return validateEmail(value) || base.Keypair.isValidPublicKey(value)
 }
 export const documentContainer = value => value instanceof DocumentContainer
-export const amountRange = (from, to) => value => Number(value) &&
-  Number(value) >= from && Number(value) <= to
+export const amountRange = (from, to) => value => {
+  return Number(value) && Number(value) >= from && Number(value) <= to
+}
 
 export * from 'vuelidate/lib/validators'
 
