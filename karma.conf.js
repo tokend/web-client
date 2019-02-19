83193d8e66753dc8b1339a9f8942842e35f4d7ce
Vladislav <logvinenko.js@gmail.com>
2019-02-19 15:23:52 +0200

    Restore karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -17,12 +17,12 @@ module.exports = function (config) {
       },
     },
     files: [
-      'src/vue/modules/**/*.spec.js',
-      // 'src/**/*.e2e.js',
+      'src/**/*.spec.js',
+      'src/**/*.e2e.js',
     ],
     preprocessors: {
       'src/**/*.spec.js': [ 'webpack' ],
-      // 'src/**/*.e2e.js': [ 'webpack' ],
+      'src/**/*.e2e.js': [ 'webpack' ],
     },
 
     webpack: webpackConfig,
