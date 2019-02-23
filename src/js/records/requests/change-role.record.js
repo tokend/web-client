9280a6e4fab5a89741f36e9631af5f400e017833
ardier16 <ardier16@gmail.com>
2019-02-23 14:40:16 +0200

    Update SDK

--- src/js/records/requests/change-role.record.js
+++ src/js/records/requests/change-role.record.js
@@ -10,7 +10,7 @@ export class ChangeRoleRequestRecord extends RequestRecord {
     )
 
     this.accountRoleToSet = _get(record, 'requestDetails.accountRoleToSet')
-    this.blobId = _get(record, 'requestDetails.kycData.blobId')
+    this.blobId = _get(record, 'requestDetails.creatorDetails.blobId')
     this.externalDetails = _get(record, 'externalDetails.data')
   }
 
