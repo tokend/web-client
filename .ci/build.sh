bf6d3476ad23a11e0ed42d3492423c4b0af19171
Pavel Prysiazhniuk <38535763+iamp3@users.noreply.github.com>
2019-02-27 13:19:54 +0200

    Update build.sh

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-docker build --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
+docker build --build-arg BUILD_VERSION="$CI_COMMIT_TAG" --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
 docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA
