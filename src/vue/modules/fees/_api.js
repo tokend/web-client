dfd7776a4d4e418aaadade8bff6c5305c8055af5
ardier16 <ardier16@gmail.com>
2019-03-01 11:40:54 +0200

    Add fees module
    + fees module structure
    + assets loading
    + insert fees module into fees page

---
+++ src/vue/modules/fees/_api.js
@@ -0,0 +1,25 @@
+import { ApiCaller } from '@tokend/js-sdk'
+
+let _api = null
+
+/**
+ * @param {Object} config - wallet to sign the requests
+ * @param {String} config.horizonURL - the url of the horizon server
+ * (without version prefix)
+ */
+export function initApi (config) {
+  const horizonURL = config.horizonURL
+
+  _api = ApiCaller.getInstance(horizonURL)
+}
+
+/**
+ * @returns {sdk.ApiCaller}
+ */
+export function api () {
+  if (!_api) {
+    throw new Error('API is not initialized')
+  }
+
+  return _api
+}
---
+++ src/vue/modules/fees/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fees" />
+</template>
+
+<script>
+export default {
+  name: 'fees-module',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
