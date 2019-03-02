5bb54ce370fe6fc60d25a08d1aae11b6bbfa06ab
ardier16 <ardier16@gmail.com>
2019-03-02 12:46:32 +0200

    Add comment for filterFees function

--- src/vue/modules/fees/helpers/filter-fees.js
+++ src/vue/modules/fees/helpers/filter-fees.js
@@ -1,5 +1,7 @@
 import { Fee } from '../wrappers/fee'
 
+// HACK: fees are filtered by specific account ID and account role ID.
+// If neither of them is present the fee is considered to be general.
 export function filterFees (fees, accountId, accountRoleId) {
   return fees
     .map(f => new Fee(f))
