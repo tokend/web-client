1aae2b1473ca0f1fcd0ecc73264a7904378afd2a
Roman Malyshev <lem0000000000n@gmail.com>
2019-03-04 20:34:55 +0200

    Add coinpayments module

---
+++ src/vue/modules/coinpayments/components/coinpayments-deposit-list-item.vue
@@ -0,0 +1,93 @@
+<template>
+  <tbody class="deposit-list-item">
+    <tr
+      class="deposit-list-item__tr"
+      :class="{ 'deposit-list-item__tr--dark': index%2 }">
+      <td class="deposit-list-item__timeout-ticker">
+        <timeout-ticker :timeout="+item.timeout" />
+      </td>
+      <td class="deposit-list-item__amount">
+        {{ { value: item.amount, currency: item.asset } | formatMoney }}
+      </td>
+      <td class="deposit-list-item__btn">
+        <button
+          class="app__button-icon"
+          @click="isAddressViewerShown = !isAddressViewerShown">
+          <i class="mdi mdi-view-dashboard" />
+        </button>
+      </td>
+    </tr>
+    <tr class="deposit-list-item__key-viewer-wrp" v-if="isAddressViewerShown">
+      <td colspan="3">
+        <key-viewer
+          class="deposit-list-item__key-viewer"
+          :value="item.address"
+        />
+      </td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+import TimeoutTicker from './coinpayments-timeout-ticker'
+import KeyViewer from '@/vue/common/KeyViewer'
+export default {
+  name: '',
+  components: {
+    TimeoutTicker,
+    KeyViewer,
+  },
+  mixins: [],
+  props: {
+    item: { type: Object, required: true },
+    index: { type: Number, required: true },
+  },
+  data () {
+    return {
+      isAddressViewerShown: false,
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+  },
+  destroyed () {
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@scss/variables';
+
+  .deposit-list-item {
+      td{
+        padding: 0.5rem 1rem;
+      }
+    }
+
+  .deposit-list-item__tr{
+    &:hover{
+      background: $col-table-row-selected;
+    }
+  }
+
+  .deposit-list-item__tr--dark {
+    background: $col-table-alt-row-background;
+  }
+
+  .deposit-list-item__amount {
+    max-width: 24rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: right;
+  }
+
+  .deposit-list-item__key-viewer {
+    padding: 2rem;
+  }
+
+  .deposit-list-item__btn {
+    width: 3.5rem;
+  }
+</style>
---
+++ src/vue/modules/coinpayments/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="coinpayments">
+    <coinpayments-form
+      :asset="asset"
+    />
+    <div class="coinpayments__deposit-list-wrp">
+      <deposit-list />
+    </div>
+  </div>
+</template>
+
+<script>
+import coinpaymentsForm from './components/coinpayments-form'
+import DepositList from './components/coinpayments-deposit-list'
+
+export default {
+  name: '',
+  components: {
+    coinpaymentsForm,
+    DepositList,
+  },
+  mixins: [],
+  props: {
+    asset: { type: Object, required: true },
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  watch: {},
+  created () {
+  },
+  destroyed () {
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+  .coinpayments__address-viewer-wrp {
+    margin-top: 5rem;
+  }
+
+  .coinpayments__deposit-list-wrp {
+    margin-top: 5rem;
+  }
+</style>
