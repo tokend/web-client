c010ad67d01b493bf5327273d130f3e9bcfb072e
templar15.2012 <templar15.2012@gmail.com>
2019-03-06 13:32:08 +0300

    test new build.sh

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,5 +2,11 @@
 
 set -ex
 
-docker build --build-arg BUILD_VERSION=$CI_COMMIT_TAG --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
+if [ -z "$CI_COMMIT_TAG" ]
+then
+  docker build --build-arg BUILD_VERSION=$CI_COMMIT_TAG --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
+else
+  docker build --build-arg BUILD_VERSION=$CI_COMMIT_REF_NAME --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
+fi
+
 docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA
