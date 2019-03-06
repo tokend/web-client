48ddbf4beb70187d256b1b13bf31f54f835a64cf
Roman Malyshev <lem0000000000n@gmail.com>
2019-03-06 19:22:54 +0200

    Started add test

---
+++ src/vue/modules/coinpayments/components/coinpayments-form.spec.js
@@ -0,0 +1,81 @@
+import CoinpaymentsForm from './coinpayments-form'
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Vuelidate from 'vuelidate'
+
+import { createLocalVue, mount } from '@vue/test-utils'
+
+import { types } from '../store/types'
+import { coinpaymentsModule } from '../store/index'
+
+Vue.config.silent = true
+
+const localVue = createLocalVue()
+localVue.use(Vuelidate)
+localVue.use(Vuex)
+
+describe('CoinpaymentsForm', () => {
+  const depositDetails = {
+    address: 'ADRESS',
+    amount: '123',
+    asset: 'BTC',
+    confirms_needed: '2',
+    timeout: +new Date() + 9000,
+    txn_id: '9379992',
+  }
+  const sampleCoinpaymentsFormData = {
+    form: {
+      amount: 10,
+    },
+    isFailed: false,
+  }
+
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  describe('validation rules assigned correctly', () => {
+    let wrapper
+
+    beforeEach(() => {
+      const actions = coinpaymentsModule.actions
+
+      sinon.stub(actions, types.LOAD_DEPOSIT).returns(depositDetails)
+
+      const store = new Vuex.Store({
+        actions,
+      })
+      wrapper = mount(CoinpaymentsForm, {
+        localVue,
+        data: () => Object.assign({}, sampleCoinpaymentsFormData),
+        sync: false,
+        store,
+      })
+    })
+
+    const expectedResults = {
+      amount: ['required', 'amountRange'],
+    }
+
+    for (const [model, rules] of Object.entries(expectedResults)) {
+      it(`${model} model is validating by proper set of rules`, () => {
+        expect(Object.keys(wrapper.vm.$v.form[model].$params))
+          .to.deep.equal(rules)
+      })
+    }
+
+    const fieldBindings = {
+      '#coinpayments-amount': 'amount',
+    }
+
+    for (const [selector, model] of Object.entries(fieldBindings)) {
+      it(`$v.form.${model} is touched after blur event emitted on ${selector}`, () => {
+        const spy = sinon.stub(wrapper.vm, 'touchField')
+        wrapper.find(selector).vm.$emit('blur')
+
+        expect(spy.calledOnce).to.be.true
+      })
+    }
+  })
+})
