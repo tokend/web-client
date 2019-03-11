868338a78c8b5dc75033e0814eb8b3d471b2535c
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-03-11 14:59:59 +0200

    Add some docs to page-module

--- src/modules-arch/page-module.js
+++ src/modules-arch/page-module.js
@@ -4,20 +4,46 @@ import { ModuleDescriptor } from './module-descriptor'
 export class PageModule extends ModuleDescriptor {
   /**
    * @param {Object} pageOpts
-   * @param {Object} routerEntry Vue router entry
+   * @param {Object} pageOpts.routerEntry
+   * Vue router entry.
+   *
+   * @param {String} [pageOpts.menuButtonTranslationId]
+   * Menu button translation ID. The button wont be rendered if no translation
+   * ID provided.
+   *
+   * @param {String} [pageOpts.menuButtonMdiName]
+   * Name of the icon to be used in sidebar. Should be kebab-cased.
+   *
+   * @param {String} [pageOpts.menuSectionTranslationId]
+   * All the menu items with the same value of menuSectionTranslationId will be
+   * rendered as a group in the Sidebar. If the argument is omitted, the menu
+   * item will be rendered in default section with no label.
+   *
+   * @param {Boolean} [isCorporateOnly]
+   * If `true` only corporate accounts can access the page
+   *
    * @param {Object} moduleOpts @link ModuleDescriptor
    */
   constructor (pageOpts = {}, moduleOpts = {}) {
     super(moduleOpts)
+    this._routerEntry = {}
+    this._menuButtonTranslationId = pageOpts.menuButtonTranslationId || ''
+    this._menuButtonMdiName = pageOpts.menuButtonMdiName || ''
+    this._menuSectionTranslationId = pageOpts.menuSectionTranslationId || ''
+    this._isCorporateOnly = pageOpts.isCorporateOnly || false
 
-    this._parsePageOpts(pageOpts)
+    this._parseRouterEntry(pageOpts)
   }
 
   get routerEntry () { return _cloneDeep(this._routerEntry) }
+  get menuButtonTranslationId () { return this._menuButtonTranslationId }
+  get menuButtonMdiName () { return this._menuButtonMdiName }
+  get menuSectionTranslationId () { return this._menuSectionTranslationId }
+  get isCorporateOnly () { return this._isCorporateOnly }
 
-  _parsePageOpts (pageOpts = {}) {
+  _parseRouterEntry (pageOpts = {}) {
     if (!pageOpts.routerEntry) {
-      throw new Error(`${this.constructor.name}._parsePageOpts: no pageOpts.routerEntry provided!`)
+      throw new Error(`${this.constructor.name}: no pageOpts.routerEntry provided!`)
     }
 
     const entry = _cloneDeep(pageOpts.routerEntry)
