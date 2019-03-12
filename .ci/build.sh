28c00c3092924130e9a60770cdf653aa4a68f52b
Pavlo Ponomarov <i.haz.teh.codez@gmail.com>
2019-03-12 10:11:12 +0200

    Hopefully fix something

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,9 +4,10 @@ set -ex
 
 if [ -z "$CI_COMMIT_TAG" ];
 then
-  docker build --build-arg BUILD_VERSION=$CI_COMMIT_REF_NAME --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
+  BUILD_VERSION=$CI_COMMIT_SHORT_SHA
 else
-  docker build --build-arg BUILD_VERSION=$CI_COMMIT_TAG --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
+  BUILD_VERSION=$CI_COMMIT_TAG
 fi
 
+docker build --build-arg BUILD_VERSION=$CI_COMMIT_REF_NAME --pull -t $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA .
 docker push $CI_REGISTRY_IMAGE:$CI_COMMIT_SHA
