1160ec1e0aca5e014122842d8481a80bd6ecacf4
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-03-22 18:15:27 +0200

    Fix statistics a bit

--- src/vue/modules/loyalty-points/loyalty-points-statistics/index.vue
+++ src/vue/modules/loyalty-points/loyalty-points-statistics/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="loyalty-points-statistics">
     <div class="loyalty-points-statistics__overall">
-      <div class="loyalty-points-statistics_viewer">
-        <h3 class="loyalty-points-statistics_viewer-title">
+      <!-- TODO: temp. disabled -->
+      <div class="loyalty-points-statistics__viewer" v-if="false">
+        <h3 class="loyalty-points-statistics__viewer-title">
           {{ 'loyalty-points-statistics.payable-receivable-title' | globalize }}
         </h3>
         <payable-receivable-viewer />
       </div>
 
-      <div class="loyalty-points-statistics_viewer">
-        <h3 class="loyalty-points-statistics_viewer-title">
+      <div class="loyalty-points-statistics__viewer">
+        <h3 class="loyalty-points-statistics__viewer-title">
           <!-- eslint-disable-next-line max-len -->
           {{ 'loyalty-points-statistics.receivable-distribution-title' | globalize }}
         </h3>
@@ -18,8 +19,8 @@
     </div>
 
     <div class="loyalty-points-statistics__overtimes">
-      <div class="loyalty-points-statistics_viewer">
-        <h3 class="loyalty-points-statistics_viewer-title">
+      <div class="loyalty-points-statistics__viewer">
+        <h3 class="loyalty-points-statistics__viewer-title">
           {{
             'loyalty-points-statistics.receivable-overtime-title' | globalize
           }}
@@ -27,8 +28,8 @@
         <receivable-overtime-viewer />
       </div>
 
-      <div class="loyalty-points-statistics_viewer">
-        <h3 class="loyalty-points-statistics_viewer-title">
+      <div class="loyalty-points-statistics__viewer">
+        <h3 class="loyalty-points-statistics__viewer-title">
           {{ 'loyalty-points-statistics.payable-overtime-title' | globalize }}
         </h3>
         <payable-overtime-viewer />
@@ -84,13 +85,19 @@ export default {
 .loyalty-points-statistics__overtimes {
   display: flex;
   flex-wrap: wrap;
-  margin: -2rem;
+  margin: 0 -2rem;
 }
 
-.loyalty-points-statistics_viewer {
+.loyalty-points-statistics__viewer {
   margin: 2rem;
   flex: 0 1 calc(50% - 4rem);
   min-height: 100%;
+
+  &:only-child {
+    flex: 1;
+    max-width: 75rem;
+    margin: auto;
+  }
 
   @include respond-to-custom($x-medium) {
     flex: 0 1 calc(100% - 4rem);
@@ -101,7 +108,7 @@ export default {
   margin-top: 2rem;
 }
 
-.loyalty-points-statistics_viewer-title {
+.loyalty-points-statistics__viewer-title {
   margin-bottom: 1.6rem;
   font-size: 2.2rem;
   text-align: center;
