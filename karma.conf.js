cdae6b3a04f3ae0d7a3c8f1471da66e927808211
EddyRock <eduardrudnev97@gmail.com>
2019-04-08 12:45:49 +0300

    fixed test in TransferForm

--- karma.conf.js
+++ karma.conf.js
@@ -18,14 +18,13 @@ module.exports = function (config) {
     },
     files: [
       'test/disallow-console-calls.js',
-      // 'src/**/*.spec.js',
+      'src/**/*.spec.js',
       'src/vue/forms/TransferForm.spec.js',
       'src/**/*.e2e.js',
     ],
     preprocessors: {
       'test/disallow-console-calls.js': ['webpack'],
-      // 'src/**/*.spec.js': [ 'webpack' ],
-      'src/vue/forms/TransferForm.spec.js': [ 'webpack' ],
+      'src/**/*.spec.js': [ 'webpack' ],
       'src/**/*.e2e.js': [ 'webpack' ],
     },
 
