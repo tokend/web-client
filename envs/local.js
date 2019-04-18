d6f9cd620f1c7f3f0556c4f036df973787755d3e
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-04-18 20:30:35 +0300

    Remove an accident

--- envs/local.js
+++ envs/local.js
@@ -1,11 +1,6 @@
 module.exports = {
   NODE_ENV: '"default"',
-  // HORIZON_SERVER: '"http://localhost:8000/_/api/"',
-  // FILE_STORAGE: '"http://localhost:8000/_/storage/api"',
-  // NETWORK_PASSPHRASE: '"TokenD Developer Network"',
-
-  HORIZON_SERVER: "'https://api.demo.tokend.io'",
-  KEY_SERVER_ADMIN: "'https://adks.demo.tokend.io'",
-  FILE_STORAGE: "'https://s3.eu-west-1.amazonaws.com/demo-identity-storage-festive-colden'",
-  NETWORK_PASSPHRASE: "'Demo Network!'",
+  HORIZON_SERVER: '"http://localhost:8000/_/api/"',
+  FILE_STORAGE: '"http://localhost:8000/_/storage/api"',
+  NETWORK_PASSPHRASE: '"TokenD Developer Network"',
 }
