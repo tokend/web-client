631177d1d4b6d9f666cc539f430ee93cebe856c7
Alex <all.reshetnyak@gmail.com>
2019-05-06 14:12:46 +0300

    Show asset-type row on asset-details viewer

--- src/vue/modules/assets/shared/components/asset-attributes-viewer.vue
+++ src/vue/modules/assets/shared/components/asset-attributes-viewer.vue
@@ -87,29 +87,19 @@
           </tr>
           <tr>
             <td>
-              {{ 'assets.verification-required-title' | globalize }}
+              {{ 'assets.asset-type' | globalize }}
             </td>
             <td>
               <template v-if="asset.type === kycRequiredAssetType">
-                {{ 'assets.present-msg' | globalize }}
+                {{ 'assets.verification-required-title' | globalize }}
+              </template>
+
+              <template v-else-if="asset.type === securityAssetType">
+                {{ 'assets.security-asset-title' | globalize }}
               </template>
 
               <template v-else>
-                {{ 'assets.absent-msg' | globalize }}
-              </template>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              {{ 'assets.security-asset-title' | globalize }}
-            </td>
-            <td>
-              <template v-if="asset.type === securityAssetType">
-                {{ 'assets.present-msg' | globalize }}
-              </template>
-
-              <template v-else>
-                {{ 'assets.absent-msg' | globalize }}
+                {{ 'assets.does-not-require-verification-title' | globalize }}
               </template>
             </td>
           </tr>
