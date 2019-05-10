1fa2b211e95171c42806d3b0cf8730eafad8f586
ardier16 <ardier16@gmail.com>
2019-05-10 16:47:17 +0300

    Display sale participation on statistics drawer

--- src/vue/pages/sale-details/SaleStatisticsViewer.vue
+++ src/vue/pages/sale-details/SaleStatisticsViewer.vue
@@ -1,31 +1,126 @@
 <template>
   <div class="sale-statistics-viewer">
-    Statistics
+    <template v-if="isLoaded">
+      <template v-if="participations.length">
+        <h3>{{ 'sale-statistics.participation-title' | globalize }}</h3>
+        <div class="app__table">
+          <table>
+            <thead>
+              <th>{{ 'sale-statistics.participant-th' | globalize }}</th>
+              <th>{{ 'sale-statistics.amount-th' | globalize }}</th>
+            </thead>
+            <tbody>
+              <tr
+                v-for="participation in participations"
+                :key="participation.id"
+              >
+                <td>
+                  <email-getter :account-id="participation.participant.id" />
+                </td>
+                <td>
+                  {{
+                    {
+                      value: participation.amount,
+                      currency: participation.quoteAsset.id
+                    } | formatMoney
+                  }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </template>
+
+      <no-data-message
+        v-else
+        icon-name="trending-up"
+        :title="'sale-statistics.no-investments-title' | globalize"
+        :message="'sale-statistics.no-investments-msg' | globalize"
+      />
+    </template>
+
+    <template v-else-if="isLoadFailed">
+      <p class="sale-statistics-viewer-msg">
+        {{ 'sale-statistics.loading-error-msg' | globalize }}
+      </p>
+    </template>
+
+    <template v-else>
+      <load-spinner message-id="sale-statistics.loading-msg" />
+    </template>
+
+    <div class="sale-statistics-viewer__collection-loader-wrp">
+      <collection-loader
+        v-show="isLoaded && participations.length"
+        :first-page-loader="firstPageLoader"
+        @first-page-load="setParticipations"
+        @next-page-load="concatParticipations"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import EmailGetter from '@/vue/common/EmailGetter'
+import LoadSpinner from '@/vue/common/Loader'
+import NoDataMessage from '@/vue/common/NoDataMessage'
+import CollectionLoader from '@/vue/common/CollectionLoader'
+
 import { Api } from '@/api'
+import { ErrorHandler } from '@/js/helpers/error-handler'
 
 import { SaleRecord } from '@/js/records/entities/sale.record'
 
 export default {
   name: 'sale-statictics-viewer',
+  components: {
+    EmailGetter,
+    LoadSpinner,
+    NoDataMessage,
+    CollectionLoader,
+  },
 
   props: {
     sale: { type: SaleRecord, required: true },
   },
 
-  async created () {
-    await this.loadSaleParticipants()
+  data: _ => ({
+    isLoaded: false,
+    isLoadFailed: false,
+    participations: [],
+    firstPageLoader: _ => {},
+  }),
+
+  created () {
+    this.initFirstPageLoader()
   },
 
   methods: {
+    initFirstPageLoader () {
+      this.firstPageLoader = _ => this.loadSaleParticipants()
+    },
+
     async loadSaleParticipants () {
-      const endpoint = `/v3/sales/${this.sale.id}/relationships/participation`
-      const { data } = await Api.getWithSignature(endpoint)
-      console.log(data)
-    }
-  }
+      this.isLoaded = false
+      try {
+        const endpoint = `/v3/sales/${this.sale.id}/relationships/participation`
+        const response = await Api.getWithSignature(endpoint)
+        this.isLoaded = true
+
+        return response
+      } catch (e) {
+        this.isLoadFailed = true
+        ErrorHandler.processWithoutFeedback()
+      }
+    },
+
+    setParticipations (participations) {
+      this.participations = participations
+    },
+
+    concatParticipations (participations) {
+      this.participations = this.participations.concat(participations)
+    },
+  },
 }
 </script>
