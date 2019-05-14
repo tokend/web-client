e3d1446b461393555809088b0bd6b1b8952b3be2
ardier16 <ardier16@gmail.com>
2019-05-14 16:48:20 +0300

    Add registered users table stub

--- src/vue/pages/sale-details/SaleWhitelistManager.vue
+++ src/vue/pages/sale-details/SaleWhitelistManager.vue
@@ -2,24 +2,24 @@
   <div class="sale-statistics-viewer">
     <tabs>
       <tab
-        :name="'sale-whitelist-manager.invite-tab' | globalize"
+        :name="'sale-whitelist.invite-tab' | globalize"
         id="sale-whitelist-manager-invite-tab"
       >
         Invite tab
       </tab>
 
       <tab
-        :name="'sale-whitelist-manager.unregistered-users-tab' | globalize"
+        :name="'sale-whitelist.unregistered-users-tab' | globalize"
         id="sale-whitelist-manager-unregistered-users-tab"
       >
         Unregistered users
       </tab>
 
       <tab
-        :name="'sale-whitelist-manager.registered-users-tab' | globalize"
+        :name="'sale-whitelist.registered-users-tab' | globalize"
         id="sale-whitelist-manager-registered-users-tab"
       >
-        Registered users
+        <sale-whitelist-registered-users :sale="sale" />
       </tab>
     </tabs>
   </div>
@@ -28,6 +28,7 @@
 <script>
 import Tabs from '@/vue/common/tabs/Tabs'
 import Tab from '@/vue/common/tabs/Tab'
+import SaleWhitelistRegisteredUsers from './whitelist/SaleWhitelistRegisteredUsers'
 
 import { SaleRecord } from '@/js/records/entities/sale.record'
 
@@ -36,6 +37,7 @@ export default {
   components: {
     Tabs,
     Tab,
+    SaleWhitelistRegisteredUsers,
   },
 
   props: {
---
+++ src/vue/pages/sale-details/whitelist/UserTableActions.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="user-table-actions">
+    <template v-if="isConfirmationShown">
+      <button
+        class="app__button-raised user-table-actions__btn"
+        @click="$emit(EVENTS.removeClick)"
+      >
+        Confirm
+      </button>
+
+      <button
+        class="app__button-flat user-table-actions__btn"
+        @click="isConfirmationShown = false"
+      >
+        Cancel
+      </button>
+    </template>
+
+    <template v-else>
+      <button
+        class="app__button-raised user-table-actions__btn"
+        @click="isConfirmationShown = true"
+      >
+        Remove
+      </button>
+    </template>
+  </div>
+</template>
+
+<script>
+const EVENTS = {
+  removeClick: 'remove-click',
+}
+
+export default {
+  name: 'user-table-actions',
+
+  data: _ => ({
+    isConfirmationShown: false,
+    EVENTS,
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+.user-table-actions {
+  display: flex;
+  justify-content: flex-end;
+  margin: 0 -0.6rem;
+}
+
+.user-table-actions__btn {
+  margin: 0 0.6rem;
+}
+</style>
