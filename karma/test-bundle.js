1e3e7b474fb41ee1f0dfea3d77f72029e9aab776
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-06-25 03:06:01 +0300

    Do not limit chai output

--- karma/test-bundle.js
+++ karma/test-bundle.js
@@ -1,3 +1,7 @@
+// Configure chai
+// eslint-disable-next-line no-undef
+chai.config.truncateThreshold = 0
+
 // load all specs and test assets into one bundle
 const disallowConsoleCalls = require.context('.', true, /disallow-console-calls\.js$/)
 disallowConsoleCalls.keys().forEach(disallowConsoleCalls)
