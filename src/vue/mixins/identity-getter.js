1ec92dcef2d2cfe068ad4946fe872e61793f9ce7
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-06-25 15:52:18 +0300

    Extend identity-getter mixin
    - getAccountIdByEmail now accepts defaultValue to return if the account
    not found
    - add getAccountIdByEmailMass

--- src/vue/mixins/identity-getter.js
+++ src/vue/mixins/identity-getter.js
@@ -3,22 +3,43 @@ import { errors } from '@/js/errors'
 
 export default {
   methods: {
-    async getAccountIdByEmail (email) {
+    /**
+     * Fetches an account id by email
+     *
+     * @param {string} email
+     * @param {*} defaultValue Value returned if no email found. If not set an
+     * instance of `UserDoesntExistError` thrown
+     */
+    async getAccountIdByEmail (email, defaultValue) {
+      if (typeof email !== 'string') {
+        throw new Error(`getAccountIdByEmail(): 'email' arg should be a string, got ${email}`)
+      }
+
       const { data } = await api.get('/identities', {
-        filter: {
-          email: email,
-        },
-        page: {
-          limit: 1,
-        },
+        filter: { email },
+        page: { limit: 1 },
       })
 
       if (data && data[0]) {
         return data[0].address
+      } else if (defaultValue !== undefined) {
+        return defaultValue
       } else {
         throw new errors.UserDoesntExistError()
       }
     },
+
+    async getAccountIdByEmailMass (emails = []) {
+      // TODO: add an ability to return objects like { email: result }
+      // so the user can know which emails were not found
+
+      const accountIds = await Promise.all(
+        emails.map(email => this.getAccountIdByEmail(email, null))
+      )
+
+      return accountIds.filter(item => item)
+    },
+
     async getEmailByAccountId (accountId) {
       const { data } = await api.get('/identities', {
         filter: {
