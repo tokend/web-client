2e8a11c6e0081c3e5e8ead7c66bdd059bd3a1142
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-06-25 17:35:37 +0300

    Extend identity getter balance getters
    + wih getBalanceIdByEmail and getBalanceIdByEmailMass

--- src/vue/mixins/identity-getter.js
+++ src/vue/mixins/identity-getter.js
@@ -1,18 +1,21 @@
 import { api } from '@/api'
 import { errors } from '@/js/errors'
+import AccountGetterMixin from './account-getter'
 
 export default {
+  mixins: [AccountGetterMixin],
+
   methods: {
     /**
      * Fetches an account id by email
      *
      * @param {string} email
-     * @param {*} defaultValue Value returned if no email found. If not set an
+     * @param {*} [defaultValue] Value returned if no email found. If not set an
      * instance of `UserDoesntExistError` thrown
      */
     async getAccountIdByEmail (email, defaultValue) {
       if (typeof email !== 'string') {
-        throw new Error(`getAccountIdByEmail(): 'email' arg should be a string, got ${email}`)
+        throw new TypeError(`getAccountIdByEmail(): 'email' arg should be a string, got ${email}`)
       }
 
       const { data } = await api.get('/identities', {
@@ -29,6 +32,13 @@ export default {
       }
     },
 
+    /**
+     * Fetches an array of account IDs. If no account ID found for a provided
+     * email, the result will not contain this entry.
+     *
+     * @param {string[]} emails
+     * @returns {string[]} array of account IDs
+     */
     async getAccountIdByEmailMass (emails = []) {
       // TODO: add an ability to return objects like { email: result }
       // so the user can know which emails were not found
@@ -40,14 +50,39 @@ export default {
       return accountIds.filter(item => item)
     },
 
+    /**
+     * Fetches first balance ID of asset code of account with the provided email
+     *
+     * @param {string} email
+     * @param {string} assetCode A valid asset code
+     * @param {*} [defaultValue] Value returned if no balance ID found.
+     * If not set an instance of `BalanceNotFoundError` or
+     * `UserDoesntExistError` thrown
+     */
+    async getBalanceIdByEmail (email, assetCode, defaultValue) {
+      const accId = await this.getAccountIdByEmail(email, defaultValue)
+      const balId = await this.getBalanceId(accId, assetCode, defaultValue)
+      return balId
+    },
+
+    /**
+     * Fetches an array of balance IDs. If no balance ID found for a provided
+     * email, the result will not contain this entry.
+     *
+     * @param {string[]} emails
+     * @param {string} assetCode A valid asset code
+     * @returns {string[]} array of account IDs
+     */
+    async getBalanceIdByEmailMass (emails, assetCode) {
+      const accIds = await this.getAccountIdByEmailMass(emails)
+      const balIds = await this.getBalanceIdMass(accIds, assetCode)
+      return balIds
+    },
+
     async getEmailByAccountId (accountId) {
       const { data } = await api.get('/identities', {
-        filter: {
-          address: accountId,
-        },
-        page: {
-          limit: 1,
-        },
+        filter: { address: accountId },
+        page: { limit: 1 },
       })
 
       if (data && data[0]) {
