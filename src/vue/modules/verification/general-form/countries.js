7b77a15c6ae81e1517d2070387714da968de56b8
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-07-12 18:56:41 +0300

    Add globalizeCountry

--- src/vue/modules/verification/general-form/countries.js
+++ src/vue/modules/verification/general-form/countries.js
@@ -1,18 +1,5 @@
-import { getI18nNamespace, onI18nLanguageChange } from '@/i18n/index'
 import { byAlpha2 } from 'iso-country-codes'
 
-function getCountries () {
-  const translations = getI18nNamespace('countries')
-
-  return Object
-    .keys(byAlpha2 || {})
-    .map(code => ({ code, translation: translations[code] })) || []
-}
-
-let COUNTRIES = getCountries()
-
-onI18nLanguageChange(_ => {
-  COUNTRIES = getCountries()
-})
+let COUNTRIES = Object.keys(byAlpha2 || {})
 
 export { COUNTRIES }
