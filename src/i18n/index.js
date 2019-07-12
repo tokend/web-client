32455a1c73e56842c7441e2fa481e8ab22330911
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-07-12 17:40:03 +0300

    Refactor i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,11 @@
-// TODO: remove
-/* eslint-disable no-unused-vars */
-
 import i18next from 'i18next'
 import i18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
 
 // i18next api:
 // https://www.i18next.com/overview/api
+
+// list of iso639 codes on wiki
+// https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 
 import moment from 'moment-timezone'
 
@@ -15,216 +15,202 @@ import _merge from 'lodash/merge'
 
 import { MathUtil } from '@/js/utils'
 
-async function loadLngResources (lang) {
-  let result
-
-  switch (lang) {
-    case 'en':
-      result = _merge(
-        (await import(`./resources/en`)).default,
-        (await import(`./resources/en.terms`)).default,
-        (await import(`./resources/en.pre-issuance-guide`)).default,
-        { countries: (await import(`./resources/en.countries`)).default },
-      )
-      break
-
-    case 'ru':
-      result = _merge(
-        (await import(`./resources/ru`)).default,
-        (await import(`./resources/ru.terms`)).default,
-        (await import(`./resources/ru.pre-issuance-guide`)).default,
-        { countries: (await import(`./resources/ru.countries`)).default },
-      )
-      break
-
-    default:
-      throw new Error(`loadLngResources: unknown language: ${lang}`)
+class I18n {
+  constructor () {
+    this._i18nextInstance = i18next.createInstance()
+    this._i18nextConfig = this._buildDefaultConfig()
+    this._languageChangeListeners = []
   }
 
-  return result
-}
+  async init () {
+    await this._i18nextInstance
+      .use(i18nextBrowserLanguageDetector)
+      .init(this._i18nextConfig)
 
-function parseResource (resource) {
-  if (!_isObject(resource)) {
-    throw new TypeError(`parseResource(): \`resource\` should be an object, got ${typeof resource}`)
+    // on init, i18n detected and set language, now we should apply it
+    await this.changeLanguage(this.language)
   }
 
-  let result = {}
-
-  if (resource.translations) {
-    result.translations = resource.translations
-  }
-
-  if (resource.config) {
-    result.config = resource.config
-  }
-
-  if (resource.countries) {
-    result.countries = resource.countries
-  }
-
-  return result
-}
-
-function appendLngResources (lang, ...resources) {
-  for (const resource of resources) {
-    const parsed = parseResource(resource)
-
-    console.log('appendLngResources: lng', lang)
-    console.log('appendLngResources: parsed', parsed)
-
-    if (parsed.translations) {
-      // i18next.addResources(lang, 'translation', parsed.translations)
-      i18next.addResourceBundle(lang, 'translation', parsed.translations,
-        true, true
-      )
+  async changeLanguage (lng) {
+    if (!this._i18nextInstance.options.resources[lng]) {
+      const lngResources = await this._loadResources(lng)
+      this._appendResources(lng, lngResources)
     }
 
-    if (parsed.config) {
-      i18next.addResourceBundle(lang, 'config', parsed.config, true, true)
+    this._i18nextInstance.changeLanguage(lng)
+    this._languageChangeListeners.forEach(cb => cb(lng))
+  }
+
+  async _loadResources (lang) {
+    let result
+
+    // TODO: replace require.ensures with imports
+
+    switch (lang) {
+      case 'en':
+        result = _merge(
+          await require.ensure([], require => require('./resources/en')),
+          await require.ensure([], require => require('./resources/en.terms')),
+          await require.ensure([], require => require('./resources/en.pre-issuance-guide')),
+          await require.ensure([], require => require('./resources/en.countries')),
+        )
+        break
+
+      case 'ru':
+        result = _merge(
+          await require.ensure([], require => require('./resources/ru')),
+          await require.ensure([], require => require('./resources/ru.terms')),
+          await require.ensure([], require => require('./resources/ru.pre-issuance-guide')),
+          await require.ensure([], require => require('./resources/ru.countries')),
+        )
+        break
+
+      default:
+        throw new Error(`I18n._loadResources(): unknown language: ${lang}`)
     }
 
-    if (parsed.countries) {
-      i18next.addResourceBundle(lang, 'countries', parsed.countries, true, true)
+    return result
+  }
+
+  _appendResources (lang, ...resources) {
+    for (const resource of resources) {
+      const parsed = this._parseResource(resource)
+
+      if (parsed.translations) {
+        this._i18nextInstance.addResourceBundle(
+          lang, 'translation', parsed.translations, true, true
+        )
+      }
+
+      if (parsed.config) {
+        this._i18nextInstance.addResourceBundle(
+          lang, 'config', parsed.config, true, true
+        )
+      }
+
+      if (parsed.countries) {
+        this._i18nextInstance.addResourceBundle(
+          lang, 'countries', parsed.countries, true, true
+        )
+      }
     }
   }
-}
 
-function buildI18nOptions () {
-  return {
-    fallbackLng: 'en',
-    debug: true,
-    resources: {
-      // en: {
-      //   translation: {
-      //     ...i18n.translations,
-      //   },
-      //   config: {
-      //     ...i18n.config,
-      //   },
-      // },
-      // ru: {
-      //   translation: {
-      //     ...i18n.translations,
-      //   },
-      //   config: {
-      //     ...i18n.config,
-      //   },
-      // },
-    },
-    whitelist: ['en', 'ru'],
-    // set to true if you need en-US/en-UK lng's:
-    nonExplicitWhitelist: false,
-    interpolation: {
-      format: (param, format) => {
-        const lngConfig = getI18nNamespace('config')
+  _parseResource (resource) {
+    if (!_isObject(resource)) {
+      throw new TypeError(`I18n._parseResource(): 'resource' should be an object, got ${typeof resource}`)
+    }
 
-        switch (format.toLowerCase()) {
-          case 'date':
-            return moment(param)
-              .format(_get(lngConfig, 'date.presets.datetime'))
-          case 'dmy':
-            return moment(param)
-              .format(_get(lngConfig, 'date.presets.dmy'))
-          case 'dmyt':
-            return moment(param)
-              .format(_get(lngConfig, 'date.presets.dmyt'))
-          case 'calendar':
-            return moment(param)
-              .calendar(null, _get(lngConfig, 'date.calendar'))
-          case 'calendar-inline':
-            return moment(param)
-              .calendar(null, _get(lngConfig, 'date.calendarInline'))
-          case 'money':
-            const value = (_isObject(param) ? param.value : param) || '0'
-            const defaultFormat =
-              _get(lngConfig, 'number.formats.amounts.default')
+    let result = {}
 
-            const result = MathUtil.format(value, defaultFormat)
-            return param.currency ? result.concat(' ', param.currency) : result
-          case 'number':
-            return MathUtil
-              .format(param, _get(lngConfig, 'number.formats.default'))
-          case 'integer':
-            return MathUtil
-              .format(param, _get(lngConfig, 'number.formats.integer'))
-          case 'percent':
-            const convertedPercent = MathUtil.multiply(param, 100)
-            return MathUtil.format(
-              convertedPercent,
-              _get(lngConfig, 'number.formats.percent')
-            )
-          default:
-            console.warn(`Unknown format: ${format}, skipping…`)
-            return param
-        }
+    if (resource.translations) {
+      result.translations = resource.translations
+    }
+
+    if (resource.config) {
+      result.config = resource.config
+    }
+
+    if (resource.countries) {
+      result.countries = resource.countries
+    }
+
+    return result
+  }
+
+  onLanguageChanged (listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError(`onLanguageChanged(): expect 'listener' to be a function, got ${typeof listener}`)
+    }
+
+    this._languageChangeListeners.push(listener)
+
+    // subscription remover
+    return () => {
+      this._languageChangeListeners = this._languageChangeListeners
+        .filter(item => item !== listener)
+    }
+  }
+
+  get languages () {
+    return this._i18nextInstance.options.whitelist
+      .filter(item => item !== 'cimode')
+  }
+
+  get language () {
+    return this._i18nextInstance.language
+  }
+
+  t (...args) {
+    return this._i18nextInstance.t(...args)
+  }
+
+  getFixedT (...args) {
+    return this._i18nextInstance.getFixedT(...args)
+  }
+
+  exists (...args) {
+    return this._i18nextInstance.exists(...args)
+  }
+
+  _buildDefaultConfig () {
+    return {
+      fallbackLng: 'en',
+      debug: false,
+      resources: {},
+      whitelist: ['en', 'ru'],
+      // set to true if you need en-US/en-UK lng's:
+      nonExplicitWhitelist: false,
+      interpolation: {
+        format: (param, format) => {
+          const lngConfig = this._i18nextInstance
+            .getResourceBundle(this.language, 'config')
+
+          switch (format.toLowerCase()) {
+            case 'date':
+              return moment(param)
+                .format(_get(lngConfig, 'date.presets.datetime'))
+            case 'dmy':
+              return moment(param)
+                .format(_get(lngConfig, 'date.presets.dmy'))
+            case 'dmyt':
+              return moment(param)
+                .format(_get(lngConfig, 'date.presets.dmyt'))
+            case 'calendar':
+              return moment(param)
+                .calendar(null, _get(lngConfig, 'date.calendar'))
+            case 'calendar-inline':
+              return moment(param)
+                .calendar(null, _get(lngConfig, 'date.calendarInline'))
+            case 'money':
+              const value = (_isObject(param) ? param.value : param) || '0'
+              const defaultFormat =
+                _get(lngConfig, 'number.formats.amounts.default')
+
+              const result = MathUtil.format(value, defaultFormat)
+              return param.currency
+                ? result.concat(' ', param.currency)
+                : result
+            case 'number':
+              return MathUtil
+                .format(param, _get(lngConfig, 'number.formats.default'))
+            case 'integer':
+              return MathUtil
+                .format(param, _get(lngConfig, 'number.formats.integer'))
+            case 'percent':
+              const convertedPercent = MathUtil.multiply(param, 100)
+              return MathUtil.format(
+                convertedPercent,
+                _get(lngConfig, 'number.formats.percent')
+              )
+            default:
+              console.warn(`Unknown format: ${format}, skipping…`)
+              return param
+          }
+        },
       },
-    },
+    }
   }
 }
 
-export async function initI18n () {
-  // i18next.on('initialized', arg => console.log('init:', arg))
-  onI18nLanguageChange(async lng => {
-    const lngResources = await loadLngResources(lng)
-    appendLngResources(lng, lngResources)
-    // i18next.addResource(lng, lngResources)
-
-    console.log('changed:', lng)
-    console.log('lngResources:', lngResources)
-    console.log(i18next.getDataByLanguage('en'))
-    console.log(i18next.getResourceBundle('en', 'config'))
-    console.log(i18next.languages)
-    console.log(i18next.language)
-  })
-
-  i18next
-    .use(i18nextBrowserLanguageDetector)
-    .init(
-      buildI18nOptions(
-        await loadLngResources('en')
-      )
-    )
-
-  console.dir(i18next)
-}
-
-const lang = 'en'
-export const i18nOptions = buildI18nOptions(
-  lang,
-  loadLngResources(lang)
-)
-
-export function mergeIntoI18nOptions (...localesJson) {
-  Object.assign(
-    i18nOptions,
-    buildI18nOptions(
-      lang,
-      loadLngResources(lang, ...localesJson)
-    )
-  )
-}
-
-/**
- * Returns resource bundle of a namespace
- *
- * @param {string} namespace allowed values: translation, config, countries
- * @param {string} [language] if not set, the current one will be used
- */
-export function getI18nNamespace (namespace, language = '') {
-  if (!['translation', 'config', 'countries'].includes(namespace)) {
-    throw new Error(`getI18nNamespace(): unknown namespace ${namespace}`)
-  }
-
-  const lng = language || i18next.language
-
-  if (i18next.isInitialized) {
-    return i18next.getResourceBundle(lng, namespace)
-  }
-
-  return {}
-}
-
-export function onI18nLanguageChange (handler) {
-  i18next.on('languageChanged', handler)
-}
+export const i18n = new I18n()
