57d582f92d6e34f2c6b57efbf117e783c18c56cf
Alex <all.reshetnyak@gmail.com>
2019-07-29 14:43:53 +0300

    Hotfix

--- src/js/records/entities/balance.record.js
+++ src/js/records/entities/balance.record.js
@@ -3,17 +3,22 @@ import safeGet from 'lodash/get'
 import config from '@/config'
 
 export class BalanceRecord {
-  constructor (record = {}) {
+  constructor (record = {}, precision) {
     this._record = record
 
     this.id = record.id
     this.asset = new AssetRecord(record.asset)
 
-    let availableBalance = safeGet(record, 'state.available')
-    this.balance = (+availableBalance)
-      ? String(Math.floor(parseFloat(availableBalance) * 100) / 100)
-      : parseFloat(availableBalance).toFixed(config.DECIMAL_POINTS)
+    this.balance =
+      this.balanceToPrecision(safeGet(record, 'state.available'), precision)
 
     this.convertedBalance = record.convertedBalance
+  }
+
+  balanceToPrecision (balance, precision = 0) {
+    const digits = Math.pow(10, precision || config.DECIMAL_POINTS)
+    return (+balance)
+      ? String(Math.floor(parseFloat(balance) * digits) / digits)
+      : parseFloat(balance).toFixed(precision || config.DECIMAL_POINTS)
   }
 }
