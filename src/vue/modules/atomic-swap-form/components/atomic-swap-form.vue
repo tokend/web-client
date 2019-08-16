e57a786006f8efbbf55787019fd52455af27738c
LemonJenkins <lem0000000000n@gmail.com>
2019-08-16 19:59:55 +0300

    Fix form successful msg

--- src/vue/modules/atomic-swap-form/components/atomic-swap-form.vue
+++ src/vue/modules/atomic-swap-form/components/atomic-swap-form.vue
@@ -209,7 +209,6 @@ export default {
         const createAtomicSwapBidOperation =
           this.buildCreateAtomicSwapBidOperation()
         await api.postOperations(createAtomicSwapBidOperation)
-        this.$emit(EVENTS.submitted)
         this.intervalId = setInterval(() => {
           this.loadPendingAtomicSwapBidRequests()
         }, this.loadTickerTimeout)
@@ -257,6 +256,7 @@ export default {
           clearInterval(this.intervalId)
           this.isSubmitting = false
           this.hideConfirmation()
+          this.$emit(EVENTS.submitted)
         }
       } catch (e) {
         this.isFailedLoadAtomicSwapBidRecord = true
