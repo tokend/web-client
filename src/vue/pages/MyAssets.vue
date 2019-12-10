92924412d32f06849fbfd94298fe8b067edc23e8
Roman Malyshev <lem0000000000n@gmail.com>
2019-12-10 14:01:52 +0200

    Remove SubmoduleImporter from `MyAssets` component

--- src/vue/pages/MyAssets.vue
+++ src/vue/pages/MyAssets.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="my-assets-explorer">
-    <template v-if="getModule().canRenderSubmodule(MyAssetsExplorerModule)">
-      <submodule-importer
-        :submodule="getModule().getSubmodule(MyAssetsExplorerModule)"
-        :default-quote-asset="defaultQuoteAsset"
-      />
-    </template>
+    <my-assets-explorer-module
+      :default-quote-asset="defaultQuoteAsset"
+    />
   </div>
 </template>
 
 <script>
-import SubmoduleImporter from '@/modules-arch/submodule-importer'
-import { MyAssetsExplorerModule } from '@modules/assets/my-assets-explorer/module'
+import MyAssetsExplorerModule from '@modules/assets/my-assets-explorer'
 
 import { vuexTypes } from '@/vuex'
 import { mapGetters } from 'vuex'
@@ -19,12 +15,8 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'my-assets-explorer-page',
   components: {
-    SubmoduleImporter,
-  },
-
-  data: _ => ({
     MyAssetsExplorerModule,
-  }),
+  },
 
   computed: {
     ...mapGetters({
