6efbf7b14f5940beb219ad87d88d5a5a14871726
Electr1Xx <aleksandrplotnikov1997@gmail.com>
2019-12-10 16:47:18 +0200

    removed submodule

--- src/vue/pages/polls/PollRequests.vue
+++ src/vue/pages/polls/PollRequests.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="poll-requests-page">
-    <submodule-importer
-      v-if="getModule().canRenderSubmodule(PollRequestsModule)"
-      :submodule="getModule().getSubmodule(PollRequestsModule)"
-    />
+    <poll-requests-module />
   </div>
 </template>
 
 <script>
-import SubmoduleImporter from '@/modules-arch/submodule-importer'
-import { PollRequestsModule } from '@/vue/modules/requests/poll-requests/module'
+import PollRequestsModule from '@/vue/modules/requests/poll-requests'
 
 export default {
   name: 'poll-requests-page',
   components: {
-    SubmoduleImporter,
-  },
-  data: _ => ({
     PollRequestsModule,
-  }),
+  },
 }
 </script>
