72d264ccb5ebfc3c88b4395f7d96f479c1b11bae
Oleksandr <oleksandrsavostian@gmail.com>
2020-02-24 16:15:54 +0200

    change min amount value to blank

--- src/vue/fields/AmountInputField.vue
+++ src/vue/fields/AmountInputField.vue
@@ -74,7 +74,7 @@ export default {
     value: { type: [Number, String], default: undefined },
     validationType: { type: String, default: '' },
     isMaxButtonShown: { type: Boolean, default: false },
-    min: { type: [Number, String], default: config.MIN_AMOUNT },
+    min: { type: [Number, String], default: '' },
     max: { type: [Number, String], default: config.MAX_AMOUNT },
   },
   data: _ => ({
