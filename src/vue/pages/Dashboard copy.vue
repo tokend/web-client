64674d2c4208e330df7856931f72ab86cfe2cc42
Oleksandr <oleksandrsavostian@gmail.com>
2020-04-13 15:02:10 +0300

    temp

---
+++ src/vue/pages/Dashboard copy.vue
@@ -0,0 +1,257 @@
+<template>
+  <div class="dashboard">
+    <template v-if="isLoaded">
+      <div class="dashboard__toolbar">
+        <asset-selector
+          class="dashboard__asset-selector"
+          :current-asset="currentAsset.code"
+          @asset-change="currentAsset.code = $event"
+          :scale="scale"
+        />
+        <div class="dashboard__actions">
+          <!-- eslint-disable-next-line max-len -->
+          <template v-if="isAccountCorporate">
+            <button
+              class="app__button-raised dashboard__action"
+              @click="createIssuanceFormIsShown = true"
+            >
+              <i class="mdi mdi-plus dashboard__plus-icon" />
+              {{ 'dashboard.create-issuance-btn' | globalize }}
+            </button>
+          </template>
+
+          <!-- eslint-disable-next-line max-len -->
+          <button
+            v-if="currentAsset.isTransferable"
+            class="app__button-raised dashboard__action"
+            @click="transferFormIsShown = true"
+          >
+            <i class="mdi mdi-send mdi-rotate-315 dashboard__send-icon" />
+            {{
+              'dashboard.send-asset-lbl' | globalize({
+                asset: currentAsset.code })
+            }}
+          </button>
+        </div>
+      </div>
+      <template v-if="currentAsset.code">
+        <div
+          v-if="currentAsset.code !== defaultQuoteAsset"
+          class="dashboard__chart"
+        >
+          <chart
+            :base-asset="currentAsset.code"
+            :quote-asset="defaultQuoteAsset"
+          />
+        </div>
+        <div
+          class="dashboard__activity"
+          v-if="currentAsset.code"
+        >
+          <movements-history-module
+            :asset-code="currentAsset.code"
+            :ref="REFS.movementsHistory"
+            :latest-activity="true"
+          />
+        </div>
+      </template>
+
+      <drawer :is-shown.sync="showDrawer">
+        <template
+          v-if="createIssuanceFormIsShown && isAccountCorporate"
+        >
+          <template slot="heading">
+            {{ 'dashboard.create-issuance-title' | globalize }}
+          </template>
+
+          <issuance-form-module
+            @issuance-created="closeDrawerAndUpdateList"
+          />
+        </template>
+
+        <template v-if="transferFormIsShown">
+          <template slot="heading">
+            {{ 'transfer-form.form-heading' | globalize }}
+          </template>
+          <transfer
+            @operation-submitted="closeDrawerAndUpdateList()"
+            :asset-to-transfer="currentAsset.code"
+          />
+        </template>
+      </drawer>
+    </template>
+  </div>
+</template>
+
+<script>
+import AssetSelector from '@/vue/pages/dashboard/Dashboard.AssetSelector.vue'
+import Transfer from '@/vue/forms/TransferForm'
+import IssuanceFormModule from '@/vue/modules/issuance-form'
+import Drawer from '@/vue/common/Drawer'
+import Chart from '@/vue/common/chart/Chart'
+import MovementsHistoryModule from '@/vue/modules/movements-history'
+
+import UpdateList from '@/vue/mixins/update-list.mixin'
+
+import { mapGetters, mapActions } from 'vuex'
+import { vuexTypes } from '@/vuex'
+
+const REFS = {
+  movementsHistory: 'movements-history',
+}
+
+export default {
+  name: 'dashboard',
+  components: {
+    AssetSelector,
+    Transfer,
+    Drawer,
+    IssuanceFormModule,
+    Chart,
+    MovementsHistoryModule,
+  },
+
+  mixins: [UpdateList],
+
+  data: () => ({
+    currentAsset: null,
+    isLoaded: false,
+    createIssuanceFormIsShown: false,
+    transferFormIsShown: false,
+    showDrawer: false,
+    scale: 'day',
+    REFS,
+  }),
+  computed: {
+    ...mapGetters([
+      vuexTypes.isAccountCorporate,
+      vuexTypes.accountBalances,
+      vuexTypes.defaultQuoteAsset,
+      // vuexTypes.transferableBalancesAssets,
+    ]),
+    // isTransferable () {
+    //   return this.transferableBalancesAssets.some(
+    //     item => item.code === this.currentAsset)
+    // },
+  },
+  watch: {
+    showDrawer (status) {
+      if (!status) {
+        this.createIssuanceFormIsShown = false
+        this.transferFormIsShown = false
+      }
+    },
+    createIssuanceFormIsShown (status) {
+      this.showDrawer = status
+    },
+    transferFormIsShown (status) {
+      this.showDrawer = status
+    },
+    async currentAsset (value) {
+      await this.$router.push({
+        query: { asset: value.code },
+      }, () => {})
+      this.loadBalances()
+    },
+  },
+  async created () {
+    await this.loadBalances()
+    this.setCurrentAsset()
+    this.listenUpdateList('dashboard:updateList', this.updateBalancesAndList)
+    this.isLoaded = true
+  },
+
+  beforeDestroy () {
+    this.resetUpdateListEvent('dashboard:updateList')
+  },
+
+  methods: {
+    ...mapActions({
+      loadBalances: vuexTypes.LOAD_ACCOUNT_BALANCES_DETAILS,
+    }),
+    setCurrentAsset (value) {
+      if (value) {
+        this.currentAsset = value
+      } else {
+        const keys = this.accountBalances.map(i => i.asset)
+        this.currentAsset =
+          keys.find(a => a.code === this.$route.query.asset) || keys[0] || ''
+      }
+    },
+
+    // TODO: find a better way to execute child’s reload-list method
+    updateMovementsHistoryList () {
+      if (!this.$refs[REFS.movementsHistory]) {
+        return
+      }
+      return this.$refs[REFS.movementsHistory].$children[0]
+        .reloadCollectionLoader()
+    },
+    closeDrawerAndUpdateList () {
+      this.showDrawer = false
+      this.emitUpdateList('dashboard:updateList')
+    },
+    updateBalancesAndList () {
+      return Promise.all([
+        this.loadBalances(),
+        this.updateMovementsHistoryList(),
+      ])
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@scss/variables';
+@import '~@scss/mixins';
+
+.dashboard {
+  flex: 1;
+}
+
+.dashboard__toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: -1rem;
+
+  @include respond-to($small) {
+    flex-direction: column-reverse;
+  }
+}
+
+.dashboard__actions {
+  display: flex;
+  margin: 1.8rem 1rem 1rem;
+}
+
+.dashboard__plus-icon,
+.dashboard__send-icon {
+  font-size: 1.6rem;
+  margin-right: 0.5rem;
+}
+
+.dashboard__send-icon {
+  margin-top: -0.6rem;
+}
+
+.dashboard__asset-selector {
+  margin: 1rem;
+}
+
+.dashboard__action {
+  &:not(:first-child) {
+    margin-left: 0.8rem;
+  }
+}
+
+.dashboard__chart {
+  margin-top: -4rem;
+}
+
+.dashboard__activity {
+  width: 100%;
+  margin-top: 2.4rem;
+  overflow-x: auto;
+}
+</style>
