b779502592c356634c86bdf589e0f84125db069f
Maksym Nesterov <maxym.nes.dev@gmail.com>
2020-07-10 19:18:34 +0300

    Bump blob-helpers to api-helpers

---
+++ src/js/helpers/api-helpers.js
@@ -0,0 +1,53 @@
+import { api } from '@/api'
+import { store } from '@/vuex'
+import { vuexTypes } from '@/vuex/types'
+
+export async function createPrivateBlob (type, value, blobOwnerId = '') {
+  blobOwnerId = blobOwnerId || getCurrentAccId()
+  const body = {
+    data: {
+      type,
+      attributes: { value },
+      relationships: {
+        owner: { data: { id: blobOwnerId } },
+      },
+    },
+  }
+  const { data: blob } = await api.postWithSignature('/blobs', body)
+  return blob
+}
+
+export async function getPrivateBlob (blobId, blobOwnerId = '') {
+  blobOwnerId = blobOwnerId || getCurrentAccId()
+  const endpoint = `/accounts/${blobOwnerId}/blobs/${blobId}`
+  const { data: blob } = await api.getWithSignature(endpoint)
+  return blob
+}
+
+export async function getPublicBlob (blobId) {
+  const { data: blob } = await api.getWithSignature(`/blobs/${blobId}`)
+  return blob
+}
+
+export async function getLatestRequest (endpoint, requestorId = '') {
+  requestorId = requestorId || getCurrentAccId()
+  const { data: [request] } = await api.getWithSignature(endpoint, {
+    filter: { requestor: requestorId },
+    page: { limit: 1, order: 'desc' },
+    include: ['request_details'],
+  })
+  return request
+}
+
+export async function getRequest (endpoint, reqId, requestorId = '') {
+  requestorId = requestorId || getCurrentAccId()
+  const { data: request } = await api.getWithSignature(`${endpoint}/${reqId}`, {
+    filter: { requestor: requestorId },
+    include: ['request_details'],
+  })
+  return request
+}
+
+function getCurrentAccId () {
+  return store.getters[vuexTypes.accountId]
+}
